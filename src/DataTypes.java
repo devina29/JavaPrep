0f9e0f2b8def92a99609b360beba4f9baa2af461
devina29 <devina1157@gmail.com>
2023-04-04 15:19:33 -0700

    data types

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -1,2 +1,8 @@
 public class DataTypes {
+    /**
+     * Primitive
+     * byte, short, int, long, float, double, boolean and char
+     * Non-Primitive
+     * String, Arrays and Classes
+     */
 }
